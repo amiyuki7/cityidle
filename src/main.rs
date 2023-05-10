e21aaec0314fc9ab190151dc13f61d78fe2770ac
amiyuki7 <amiyuki788@gmail.com>
2023-05-11 06:52:38 +1000

    Change logging settings

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,31 @@
-use bevy::{prelude::*, window::WindowMode};
+use bevy::{
+    log::{Level, LogPlugin},
+    prelude::*,
+    window::WindowMode,
+};
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use bevy_mod_picking::{prelude::*, selection::SelectionSettings};
 use cityidle::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "City Idle".into(),
-                mode: WindowMode::BorderlessFullscreen,
-                resizable: false,
-                ..default()
-            }),
-            ..default()
-        }))
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "City Idle".into(),
+                        mode: WindowMode::BorderlessFullscreen,
+                        resizable: false,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(LogPlugin {
+                    level: Level::TRACE,
+                    filter: "wgpu=warn,bevy_ecs=info,winit=info,naga=info,bevy_app=info,bevy_winit=info,bevy_render=info,bevy_core=info,gilrs=info"
+                        .to_string(),
+                }),
+        )
         .add_plugins(
             DefaultPickingPlugins.build(),
             // .disable::<DebugPickingPlugin>(),
@@ -31,23 +43,8 @@ fn main() {
             color: Color::WHITE,
             brightness: 0.2,
         })
-        .add_startup_systems((spawn_plane, spawn_light))
+        .add_startup_system(spawn_light)
         .run();
-}
-
-fn spawn_plane(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    commands
-        .spawn(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-            material: materials.add(Color::BLUE.into()),
-            transform: Transform::from_xyz(0.0, 0.0, 0.0),
-            ..default()
-        })
-        .insert(Name::new("Cube"));
 }
 
 fn spawn_light(mut commands: Commands) {
