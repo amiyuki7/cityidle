34c87c8a78642cae4cdf087a8ab191d09d6896b2
amiyuki7 <amiyuki788@gmail.com>
2023-05-11 12:33:50 +1000

    Add temporary building setup

--- src/grid.rs
+++ src/grid.rs
@@ -1,3 +1,4 @@
+use bevy_inspector_egui::quick::StateInspectorPlugin;
 use bevy_mod_picking::prelude::*;
 
 use crate::*;
@@ -8,21 +9,50 @@ pub struct GridPlugin;
 
 impl Plugin for GridPlugin {
     fn build(&self, app: &mut App) {
-        app.register_type::<Tile>()
+        app.add_state::<SetupState>()
+            .register_type::<SetupState>()
+            .add_plugin(StateInspectorPlugin::<SetupState>::default())
+            .register_type::<Tile>()
             .register_type::<PickSelection>()
-            .add_startup_system(spawn_grid);
+            .add_startup_system(spawn_grid)
+            .add_system(setup_buildings.run_if(state_exists_and_equals(SetupState::SpawnTileDone)));
     }
 }
 
-#[derive(Component, Reflect, Debug, Default)]
+#[derive(States, PartialEq, Eq, Debug, Clone, Hash, Default, Reflect)]
+pub enum SetupState {
+    #[default]
+    Idle,
+    SpawnTileDone,
+    SpawnBuildingDone,
+}
+
+#[derive(Component, Reflect, Debug)]
 pub struct Tile {
-    occupied: bool,
+    pub building: Option<Building>,
+    pub x: f32,
+    pub z: f32,
 }
 
 impl Tile {
-    fn new() -> Self {
-        Self::default()
+    fn new(x: f32, z: f32) -> Self {
+        Self {
+            building: None,
+            x,
+            z,
+        }
     }
+}
+
+#[derive(Reflect, FromReflect, Debug)]
+pub enum BuildingType {
+    Bank,
+}
+
+#[derive(Component, Reflect, FromReflect, Debug)]
+pub struct Building {
+    pub building_type: BuildingType,
+    pub level: u8,
 }
 
 pub fn spawn_grid(
@@ -30,6 +60,7 @@ pub fn spawn_grid(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
     asset_server: Res<AssetServer>,
+    mut next_setup_state: ResMut<NextState<SetupState>>,
 ) {
     let texture = asset_server.load("tile_texture.png");
 
@@ -60,6 +91,8 @@ pub fn spawn_grid(
             );
         }
     }
+    debug!("Finished spawning tiles");
+    next_setup_state.set(SetupState::SpawnTileDone);
 }
 
 pub fn spawn_tile(
@@ -70,16 +103,15 @@ pub fn spawn_tile(
     x_offset: u8,
     z_offset: u8,
 ) {
+    let x_pos = TILE_SIZE * x_offset as f32;
+    let z_pos = TILE_SIZE * z_offset as f32;
+
     commands
         .spawn((
             PbrBundle {
                 mesh,
                 material: default_material,
-                transform: Transform::from_xyz(
-                    TILE_SIZE * x_offset as f32,
-                    0.0,
-                    TILE_SIZE * z_offset as f32,
-                ),
+                transform: Transform::from_xyz(x_pos, 0.0, z_pos),
                 ..default()
             },
             PickableBundle::default(),
@@ -95,6 +127,34 @@ pub fn spawn_tile(
             pressed: Some(HighlightKind::Fixed(hover_material.clone())),
             selected: Some(HighlightKind::Fixed(hover_material)),
         })
-        .insert(Tile::new())
-        .insert(Name::new("Tile"));
+        .insert(Tile::new(x_pos, z_pos))
+        .insert(Name::new(format!("Tile ({x_pos},{z_pos})")));
+}
+
+fn setup_buildings(
+    mut commands: Commands,
+    mut tiles: Query<(Entity, &mut Tile)>,
+    models: Res<Models>,
+    mut next_setup_state: ResMut<NextState<SetupState>>,
+) {
+    for (tile_entity, mut tile) in tiles.iter_mut() {
+        if tile.x == 70.0 && tile.z == 60.0 {
+            let building = commands
+                .spawn(SceneBundle {
+                    scene: models.bank_scene.clone(),
+                    transform: Transform::from_scale(Vec3::splat(0.5)),
+                    ..default()
+                })
+                .id();
+
+            commands.entity(tile_entity).add_child(building);
+            tile.building = Some(Building {
+                building_type: BuildingType::Bank,
+                level: 1,
+            });
+        }
+    }
+
+    debug!("Finished setting up buildings");
+    next_setup_state.set(SetupState::SpawnBuildingDone);
 }
