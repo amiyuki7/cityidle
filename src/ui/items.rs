689371082991b353a9e72233648fff1dc43c17da
amiyuki7 <amiyuki788@gmail.com>
2023-05-13 10:09:17 +1000

    Add item icon resource loading

--- src/ui/items.rs
+++ src/ui/items.rs
@@ -1,16 +1,35 @@
+use crate::*;
+
 pub enum ItemType {
     Taffy,
-    Jellybeans,
-    Swirlmallow,
+    Nougat,
+    Marshmallow,
+}
+
+#[derive(Resource)]
+pub struct ItemIcons {
+    pub empty: Handle<Image>,
+    pub taffy: Handle<Image>,
+    pub nougat: Handle<Image>,
+    pub marshmallow: Handle<Image>,
+}
+
+pub fn load_item_icons(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.insert_resource(ItemIcons {
+        empty: asset_server.load("items/empty256.png"),
+        taffy: asset_server.load("items/taffy256.png"),
+        nougat: asset_server.load("items/nougat256.png"),
+        marshmallow: asset_server.load("items/marshmallow256.png"),
+    })
 }
 
 pub struct Item {
-    item_type: ItemType,
-    qty: u32,
+    pub item_type: ItemType,
+    pub qty: u32,
 }
 
 impl Item {
-    pub fn new_empty(item_type: ItemType) -> Self {
-        Self { item_type, qty: 0 }
+    pub fn new(item_type: ItemType, qty: u32) -> Self {
+        Self { item_type, qty }
     }
 }
